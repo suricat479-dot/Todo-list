adca0e5cd37d0febb06fe98aff244da48141a108
suricat479-dot <suricat479@gmail.com>
2026-06-05 22:25:58 +0600

    Add files via upload

--- home_page.dart
+++ home_page.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:todo/add/add_page.dart';
+import 'package:todo/database/todo.dart';
+import 'package:todo/settings/settings_page.dart';
+//import 'package:todo/main.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, 
+   required this.title,
+    required this.onToggleTheme,});
 
   final String title;
+  final Function(bool) onToggleTheme;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+    ThemeMode themeMode = ThemeMode.light;
+
+  
   List<String> tasks = [];
 
-
+  List<Todo> todoList = [
+    //mock-data
+    Todo(id: 1, title: "make dinner", isDone: true, createdAt: "01/03/2026"),
+    Todo(id: 2, title: "make lunch", isDone: true, createdAt: "02/03/2026"),
+    Todo(id: 3, title: "do hw", isDone: false, createdAt: "23/09/2026"),
+    Todo(id: 4, title: "buy new clothes", isDone: false, createdAt: "16/10/2027",),
+  ];
+  
 
   @override
   void initState() {
@@ -26,42 +42,50 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     print("Home page - build");
     return Scaffold(
-      
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
-        child: 
-        ListView.builder(
-          itemCount: tasks.length,
+        child: ListView.builder(
+          itemCount: todoList.length,
           itemBuilder: (context, index) {
-               return Container(
-      margin: EdgeInsets.all(12),
-      padding: EdgeInsets.all(16),
+            final title = todoList[index].title;
+            return Container(
+              margin: EdgeInsets.all(12),
+              padding: EdgeInsets.all(16),
 
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 243, 239, 250),
-        borderRadius: BorderRadius.circular(15),
-      ),
+              decoration: BoxDecoration(
+                
+                color: themeMode == ThemeMode.light
+              ? Color.fromARGB(255, 243, 239, 250) 
+              : Color.fromRGBO(77, 75, 80, 1),
+                borderRadius: BorderRadius.circular(15),
+              ),
 
-      child: Text(
-        tasks[index],
-        style: TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.w600,
-        ),
-      ),
-    );
+              child: ListTile(
+                
+              title: Text(title),
+
+              
+              )
+            );
+            
           },
         ),
-      ),
       
-      floatingActionButton: FloatingActionButton(
-        onPressed: _navigateToAddPage,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
+
+    
+
+      persistentFooterButtons: [
+        FloatingActionButton(
+        onPressed: _navigateToSettingsPage,
+        child: const Icon(Icons.settings),
+      ),
+      FloatingActionButton(onPressed: _navigateToAddPage,
+      child:  const Icon(Icons.add),)
+      ],
     );
   }
 
@@ -86,5 +110,10 @@ class _MyHomePageState extends State<MyHomePage> {
         tasks.insert(0, result);
       });
     }
+  }
+
+  void _navigateToSettingsPage() async {
+    Navigator.push(context, MaterialPageRoute(builder: (_) => SettingsPage(
+        onToggleTheme: widget.onToggleTheme,)));
   }
 }
