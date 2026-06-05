c0e86c38e3e783a10a15c58990c7528916a7abe2
suricat479-dot <suricat479@gmail.com>
2026-06-05 22:27:02 +0600

    Add files via upload

--- main.dart
+++ main.dart
@@ -5,16 +5,62 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  static _MyAppState of(BuildContext context) {
+    return context.findAncestorStateOfType<_MyAppState>()!;
+  }
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode themeMode = ThemeMode.light;
+
+  void toggleTheme(bool value) {
+    setState(() {
+      themeMode =
+          themeMode == ThemeMode.light
+              ? ThemeMode.dark
+              : ThemeMode.light;
+      print("themeMode $themeMode");
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage( title: 'Todo list'), 
+      title: 'Todo list',
+
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.light,
+        ),
+      ),
+
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 32, 21, 51),
+          brightness: Brightness.dark,
+        ),
+      ),
+
+      themeMode: themeMode,
+
+      home: MyHomePage(
+        title: 'Todo list',
+        onToggleTheme: toggleTheme,
+      ),
     );
   }
+
+  // void toggleTheme(bool value) {
+  //   setState(() {
+      
+  //   });
+  // }
 }
