c9861c01a508bf5bccd5b25cbab19a4ff3e714b2
suricat479-dot <suricat479@gmail.com>
2026-06-05 22:24:53 +0600

    Add files via upload

---
+++ todo.dart
@@ -0,0 +1,13 @@
+class Todo {
+  final int id;
+  final String title;
+  final bool isDone;
+  final String createdAt;
+
+  Todo({
+    required this.id,
+    required this.title,
+    required this.isDone,
+    required this.createdAt,
+  });
+}
